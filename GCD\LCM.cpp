d54538e318e8d7da0492cd1c7200cc349013012a
Daniel López <dankzer1@gmail.com>
2015-07-06 20:47:39 -0500

    Create GCD\LCM.cpp

---
+++ GCD\LCM.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+int gcd(int,int);
+int lcm(int,int);
+int main() {
+	printf("%d\n",gcd(60,20));
+	printf("%d\n",lcm(25,60));
+    return 0;
+}
+int gcd(int a,int b) {
+    if(b == 0) return a;
+    return gcd(b,a%b);
+}
+int lcm(int a,int b) {
+    return (a / gcd(a,b)) * b;
+}
