41c53f8a23e0e253e42d10a0eb70dee5fae5d36c
Daniel López <dankzer1@gmail.com>
2015-07-08 23:53:29 -0500

    Create DFS(Depth First Search).cpp

---
+++ DFS(Depth First Search).cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#define MAX 100000
+using namespace std;
+void dfs(int);
+vector <int> graph[MAX];
+int visited[MAX];
+int components;
+int main() {
+    int v,e,a,b;
+    components = 0;
+    scanf("%d %d",&v, &e);
+    while(e--) {
+        scanf("%d %d",&a, &b);
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    for(int i=1;i<=v;i++) visited[i] = 0;
+    for(int i=1;i<=v;i++) {
+        if(visited[i] == 0) {
+            dfs(i);
+            components++;
+        }
+    }
+    for(int i=1;i<=i;i++) printf("%d ",visited[i]);
+    printf("\nComponents: %d\n",components);
+    return 0;
+}
+void dfs(int u) {
+    visited[u]++;
+    for(int i=0;i<graph[u].size();i++) {
+        int v = graph[u][i];
+        if(visited[v] == 0) dfs(v);
+    }
+}
